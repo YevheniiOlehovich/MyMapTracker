d81ef5aa6214e4e2a58d3c5311de154607d0e7e1
Yevhenii Khyzhniak <keildra258@gmail.com>
2025-10-27 20:41:15 +0200

    one more try

--- frontend/src/Components/Login/index.jsx
+++ frontend/src/Components/Login/index.jsx
@@ -129,7 +129,7 @@ export default function Login({ onLogin }) {
         e.preventDefault();
         try {
             // const response = await fetch("http://localhost:5000/auth/login", {
-            const response = await fetch("http://192.168.88.153:5000/auth/login", {
+            const response = await fetch("http://192.168.88.153:5000/api/auth/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ username, password }),
