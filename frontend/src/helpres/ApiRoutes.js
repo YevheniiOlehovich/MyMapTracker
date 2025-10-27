dc40e27df12588f3f46abe8cee7149301c47f237
Yevhenii Khyzhniak <keildra258@gmail.com>
2025-10-27 15:14:50 +0200

    api routes fix

--- frontend/src/helpres/ApiRoutes.js
+++ frontend/src/helpres/ApiRoutes.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = `http://localhost:5000`;
+// const API_BASE_URL = `http://localhost:5000`;
+const API_BASE_URL = "http://192.168.88.153:5000";
 
 const apiRoutes = {
     getGroups: `${API_BASE_URL}/groups`,
