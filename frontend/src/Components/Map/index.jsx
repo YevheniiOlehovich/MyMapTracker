a5480ac5c96d753609e1d5045c842f7c10ab0b24
evhenii hyzhniak <keildra258@gmail.com>
2025-12-08 23:15:29 +0200

    gps content auto-reload

--- frontend/src/Components/Map/index.jsx
+++ frontend/src/Components/Map/index.jsx
@@ -1,12 +1,208 @@
+// import React, { useEffect, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import {
+//     MapContainer,
+//     TileLayer,
+//     useMapEvents,
+//     Polygon,
+//     Marker,
+//     Popup,
+// } from 'react-leaflet';
+// import Styles from './styled';
+// import { getTileLayerConfig } from '../../helpres/tileLayerHelper';
+// import { useCadastreData } from '../../hooks/useCadastreData';
+// import { useFieldsData } from '../../hooks/useFieldsData';
+// import { useGeozoneData } from '../../hooks/useGeozonesData';
+// import { useGpsData } from '../../hooks/useGpsData';
+// import { useUnitsData } from '../../hooks/useUnitsData';
+// import { useRentsData } from '../../hooks/useRentData';
+// import { usePropertiesData } from '../../hooks/usePropertiesData';
+// import { setActiveField, clearActiveField } from '../../store/activeFieldSlice';
+
+// import {
+//     selectShowFields,
+//     selectShowCadastre,
+//     selectShowGeozones,
+//     selectShowUnits,
+//     selectShowRent,
+//     selectShowProperty
+// } from '../../store/layersList';
+// import {
+//     selectMapCenter,
+//     selectZoomLevel,
+//     setZoomLevel,
+// } from '../../store/mapCenterSlice';
+// import { selectCurrentLocation } from '../../store/currentLocationSlice';
+// import MapCenterUpdater from '../MapCenterUpdater';
+// import TrackMarkers from '../TrackMarkers';
+// import FieldLabel from '../FieldLabel';
+// import { openAddFieldsModal, setSelectedField } from '../../store/modalSlice';
+// import L from 'leaflet';
+// import RedMarker from '../../assets/ico/redmarker.png';
+
+// import 'leaflet/dist/leaflet.css';
+// import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';
+// import '@geoman-io/leaflet-geoman-free';
+// import MeasureLayer from '../MeasureLayer'; // Імпортуємо компонент GeomanControl
+
+// import CurrentLocationMarker from '../CurrentLocationMarker';
+// import PropertyLayer from '../PropertyLayer';
+// import RentLayer from '../RentLayer';
+// import UnitsLayer from '../UnitsLayer';
+// import GeozoneLayer from '../GeozoneLayer';
+// import CadastreLayer from '../CadastreLayer';
+// import FieldsLayer from '../FieldsLayer';
+
+// function ZoomTracker({ setZoomLevel }) {
+//     useMapEvents({
+//         zoomend: (e) => {
+//             setZoomLevel(e.target.getZoom());
+//         },
+//     });
+//     return null;
+// }
+
+// export default function Map() {
+//     const dispatch = useDispatch();
+
+//     const { data: gpsData = [], isLoading: isGpsLoading, isError: isGpsError, error: gpsError } = useGpsData();
+//     const { data: cadastreData, isLoading: isCadastreLoading, error: cadastreError } = useCadastreData();
+//     const { data: fieldsData, isLoading: isFieldsLoading, error: fieldsError } = useFieldsData();
+//     const { data: geozoneData, isLoading: isGeozoneLoading, error: geozoneError } = useGeozoneData();
+//     const { data: unitsData = [], isLoading: isUnitsLoading, error: unitsError } = useUnitsData();
+//     const { data: rentData = [], isLoading: isRentsLoading, error: rentsError } = useRentsData(); // 🆕
+//     const { data: propertyData = [], isLoading: isPropertyLoading, error: propertyError } = usePropertiesData();
+
+//     const showFields = useSelector(selectShowFields);
+//     const showCadastre = useSelector(selectShowCadastre);
+//     const showGeozones = useSelector(selectShowGeozones);
+//     const showUnits = useSelector(selectShowUnits);
+//     const showRent = useSelector(selectShowRent); // 🆕
+//     const showProperty = useSelector(selectShowProperty);
+
+//     const mapType = useSelector((state) => state.map.type);
+//     const mapCenter = useSelector(selectMapCenter);
+//     const zoomLevel = useSelector(selectZoomLevel);
+//     const currentLocation = useSelector(selectCurrentLocation);
+//     const selectedDate = useSelector((state) => state.calendar.selectedDate);
+//     const selectedImei = useSelector((state) => state.vehicle.imei);
+//     const showTrack = useSelector((state) => state.vehicle.showTrack);
+
+//     const activeFieldId = useSelector((state) => state.activeField.activeFieldId);
+
+//     const [key, setKey] = useState(0);
+
+//     useEffect(() => {
+//         setKey((prevKey) => prevKey + 1);
+//     }, [mapType]);
+
+//     const tileLayerConfig = getTileLayerConfig(mapType);
+
+//     const handleEditField = (field) => {
+//         dispatch(setSelectedField(field._id));
+//         dispatch(openAddFieldsModal());
+//     };
+
+//     const currentLocationIcon = L.icon({
+//         iconUrl: RedMarker,
+//         iconSize: [25, 25],
+//         iconAnchor: [12, 12],
+//     });
+
+//     if (isGpsLoading || isCadastreLoading || isFieldsLoading || isGeozoneLoading || isUnitsLoading || isRentsLoading) {
+//         return <p>Loading map data...</p>;
+//     }
+
+//     if (isGpsError || cadastreError || fieldsError || geozoneError || unitsError || rentsError) {
+//         return (
+//             <p>
+//                 Error loading map data:{' '}
+//                 {gpsError?.message ||
+//                     cadastreError?.message ||
+//                     fieldsError?.message ||
+//                     geozoneError?.message ||
+//                     unitsError?.message ||
+//                     rentsError?.message}
+//             </p>
+//         );
+//     }
+
+//     return (
+//         <Styles.wrapper>
+//             <MapContainer
+//                 key={key}
+//                 center={mapCenter}
+//                 zoom={zoomLevel}
+//                 attributionControl={true}
+//                 doubleClickZoom={true}
+//                 scrollWheelZoom={true}
+//                 easeLinearity={0.8}
+//                 style={{ height: '100vh', width: '100%' }}
+//                 zoomControl={false}
+//             >
+//                 {tileLayerConfig && (
+//                     <TileLayer
+//                         url={tileLayerConfig.url}
+//                         subdomains={tileLayerConfig.subdomains}
+//                         attribution={tileLayerConfig.attribution}
+//                     />
+//                 )}
+
+//                 <TrackMarkers
+//                     gpsData={gpsData}
+//                     selectedDate={selectedDate}
+//                     selectedImei={selectedImei}
+//                     showTrack={showTrack}
+//                 />
+
+
+//                 {showFields && (
+//                     <FieldsLayer
+//                         fieldsData={fieldsData}
+//                         zoomLevel={zoomLevel}
+//                         activeFieldId={activeFieldId}
+//                         onEditField={handleEditField}
+//                     />
+//                 )}
+
+//                 {showCadastre && <CadastreLayer cadastreData={cadastreData} zoomLevel={zoomLevel} />}
+
+//                 {showGeozones && <GeozoneLayer geozoneData={geozoneData} zoomLevel={zoomLevel} />}
+
+//                 {showUnits && <UnitsLayer unitsData={unitsData} />}
+
+//                 {showRent && <RentLayer rentData={rentData} />}
+
+//                 {showProperty && <PropertyLayer propertyData={propertyData} zoomLevel={zoomLevel} />}
+
+//                 {currentLocation && <CurrentLocationMarker position={currentLocation} />}
+
+//                 <ZoomTracker setZoomLevel={(zoom) => dispatch(setZoomLevel(zoom))} />
+//                 <MapCenterUpdater />
+//                 <MeasureLayer />
+//             </MapContainer>
+//         </Styles.wrapper>
+//     );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
     MapContainer,
     TileLayer,
-    useMapEvents,
-    Polygon,
-    Marker,
-    Popup,
+    useMapEvents
 } from 'react-leaflet';
 import Styles from './styled';
 import { getTileLayerConfig } from '../../helpres/tileLayerHelper';
@@ -17,8 +213,7 @@ import { useGpsData } from '../../hooks/useGpsData';
 import { useUnitsData } from '../../hooks/useUnitsData';
 import { useRentsData } from '../../hooks/useRentData';
 import { usePropertiesData } from '../../hooks/usePropertiesData';
-import { setActiveField, clearActiveField } from '../../store/activeFieldSlice';
-
+import { setActiveField } from '../../store/activeFieldSlice';
 import {
     selectShowFields,
     selectShowCadastre,
@@ -32,10 +227,8 @@ import {
     selectZoomLevel,
     setZoomLevel,
 } from '../../store/mapCenterSlice';
-import { selectCurrentLocation } from '../../store/currentLocationSlice';
 import MapCenterUpdater from '../MapCenterUpdater';
 import TrackMarkers from '../TrackMarkers';
-import FieldLabel from '../FieldLabel';
 import { openAddFieldsModal, setSelectedField } from '../../store/modalSlice';
 import L from 'leaflet';
 import RedMarker from '../../assets/ico/redmarker.png';
@@ -43,8 +236,7 @@ import RedMarker from '../../assets/ico/redmarker.png';
 import 'leaflet/dist/leaflet.css';
 import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';
 import '@geoman-io/leaflet-geoman-free';
-import MeasureLayer from '../MeasureLayer'; // Імпортуємо компонент GeomanControl
-
+import MeasureLayer from '../MeasureLayer';
 import CurrentLocationMarker from '../CurrentLocationMarker';
 import PropertyLayer from '../PropertyLayer';
 import RentLayer from '../RentLayer';
@@ -53,11 +245,10 @@ import GeozoneLayer from '../GeozoneLayer';
 import CadastreLayer from '../CadastreLayer';
 import FieldsLayer from '../FieldsLayer';
 
+// Компонент для відслідковування зуму
 function ZoomTracker({ setZoomLevel }) {
     useMapEvents({
-        zoomend: (e) => {
-            setZoomLevel(e.target.getZoom());
-        },
+        zoomend: (e) => setZoomLevel(e.target.getZoom()),
     });
     return null;
 }
@@ -65,35 +256,36 @@ function ZoomTracker({ setZoomLevel }) {
 export default function Map() {
     const dispatch = useDispatch();
 
+    // Дані з нових хуків
     const { data: gpsData = [], isLoading: isGpsLoading, isError: isGpsError, error: gpsError } = useGpsData();
     const { data: cadastreData, isLoading: isCadastreLoading, error: cadastreError } = useCadastreData();
     const { data: fieldsData, isLoading: isFieldsLoading, error: fieldsError } = useFieldsData();
     const { data: geozoneData, isLoading: isGeozoneLoading, error: geozoneError } = useGeozoneData();
     const { data: unitsData = [], isLoading: isUnitsLoading, error: unitsError } = useUnitsData();
-    const { data: rentData = [], isLoading: isRentsLoading, error: rentsError } = useRentsData(); // 🆕
+    const { data: rentData = [], isLoading: isRentsLoading, error: rentsError } = useRentsData();
     const { data: propertyData = [], isLoading: isPropertyLoading, error: propertyError } = usePropertiesData();
 
+    // Селектори для відображення шарів
     const showFields = useSelector(selectShowFields);
     const showCadastre = useSelector(selectShowCadastre);
     const showGeozones = useSelector(selectShowGeozones);
     const showUnits = useSelector(selectShowUnits);
-    const showRent = useSelector(selectShowRent); // 🆕
+    const showRent = useSelector(selectShowRent);
     const showProperty = useSelector(selectShowProperty);
 
     const mapType = useSelector((state) => state.map.type);
     const mapCenter = useSelector(selectMapCenter);
     const zoomLevel = useSelector(selectZoomLevel);
-    const currentLocation = useSelector(selectCurrentLocation);
+    const currentLocation = useSelector((state) => state.currentLocationSlice.currentLocation);
     const selectedDate = useSelector((state) => state.calendar.selectedDate);
     const selectedImei = useSelector((state) => state.vehicle.imei);
     const showTrack = useSelector((state) => state.vehicle.showTrack);
-
     const activeFieldId = useSelector((state) => state.activeField.activeFieldId);
 
+    // Перезавантаження карти при зміні типу
     const [key, setKey] = useState(0);
-
     useEffect(() => {
-        setKey((prevKey) => prevKey + 1);
+        setKey((prev) => prev + 1);
     }, [mapType]);
 
     const tileLayerConfig = getTileLayerConfig(mapType);
@@ -133,9 +325,9 @@ export default function Map() {
                 key={key}
                 center={mapCenter}
                 zoom={zoomLevel}
-                attributionControl={true}
-                doubleClickZoom={true}
-                scrollWheelZoom={true}
+                attributionControl
+                scrollWheelZoom
+                doubleClickZoom
                 easeLinearity={0.8}
                 style={{ height: '100vh', width: '100%' }}
                 zoomControl={false}
@@ -148,6 +340,7 @@ export default function Map() {
                     />
                 )}
 
+                {/* Шар трекера */}
                 <TrackMarkers
                     gpsData={gpsData}
                     selectedDate={selectedDate}
@@ -155,7 +348,7 @@ export default function Map() {
                     showTrack={showTrack}
                 />
 
-
+                {/* Шари карт */}
                 {showFields && (
                     <FieldsLayer
                         fieldsData={fieldsData}
@@ -164,19 +357,16 @@ export default function Map() {
                         onEditField={handleEditField}
                     />
                 )}
-
                 {showCadastre && <CadastreLayer cadastreData={cadastreData} zoomLevel={zoomLevel} />}
-
                 {showGeozones && <GeozoneLayer geozoneData={geozoneData} zoomLevel={zoomLevel} />}
-
                 {showUnits && <UnitsLayer unitsData={unitsData} />}
-
                 {showRent && <RentLayer rentData={rentData} />}
-
                 {showProperty && <PropertyLayer propertyData={propertyData} zoomLevel={zoomLevel} />}
 
-                {currentLocation && <CurrentLocationMarker position={currentLocation} />}
+                {/* Поточне місцезнаходження */}
+                {currentLocation && <CurrentLocationMarker position={currentLocation} icon={currentLocationIcon} />}
 
+                {/* Контролери */}
                 <ZoomTracker setZoomLevel={(zoom) => dispatch(setZoomLevel(zoom))} />
                 <MapCenterUpdater />
                 <MeasureLayer />
@@ -184,7 +374,3 @@ export default function Map() {
         </Styles.wrapper>
     );
 }
-
-
-
-
--- frontend/src/hooks/useGpsData.js
+++ frontend/src/hooks/useGpsData.js
@@ -1,21 +1,26 @@
+// import { useSelector } from "react-redux";
 // import { useQuery } from '@tanstack/react-query';
 // import { fetchGpsDataApi } from '../api/gpsDataApi';
 
 // export const useGpsData = () => {
+//     const selectedDate = useSelector((state) => state.calendar.selectedDate);
+//     const year = new Date(selectedDate).getFullYear();
+
 //     return useQuery({
-//         queryKey: ['gpsData'], // Унікальний ключ для запиту
-//         queryFn: fetchGpsDataApi, // Використовуємо API-функцію
-//         staleTime: 5 * 60 * 1000, // Дані залишаються актуальними протягом 5 хвилин
-//         cacheTime: 10 * 60 * 1000, // Дані кешуються протягом 10 хвилин
-//         retry: 3, // Повторити запит 3 рази у разі помилки
+//         queryKey: ['gpsData', year],
+//         queryFn: async () => {
+//             const data = await fetchGpsDataApi(year);
+//             return data;
+//         },
+//         enabled: !!year,
+//         staleTime: 5 * 60 * 1000,
+//         cacheTime: 10 * 60 * 1000,
+//         retry: 3,
 //     });
 // };
 
 
 
-import { useSelector } from "react-redux";
-import { useQuery } from '@tanstack/react-query';
-import { fetchGpsDataApi } from '../api/gpsDataApi';
 
 export const useGpsData = () => {
     const selectedDate = useSelector((state) => state.calendar.selectedDate);
@@ -31,5 +36,7 @@ export const useGpsData = () => {
         staleTime: 5 * 60 * 1000,
         cacheTime: 10 * 60 * 1000,
         retry: 3,
+        refetchInterval: 30000,          // автооновлення кожні 30 секунд
+        refetchIntervalInBackground: true,
     });
 };
