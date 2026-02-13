c51bdb4dadbb755e88656d9dc4f1f8f0792ea5af
evhenii hyzhniak <keildra258@gmail.com>
2026-02-13 23:12:48 +0200

    styles fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.jsx';
+import "leaflet.markercluster/dist/MarkerCluster.css";
+import "leaflet.markercluster/dist/MarkerCluster.Default.css";
+import "./styles/mapMarkers.css";
 
 const queryClient = new QueryClient();
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
